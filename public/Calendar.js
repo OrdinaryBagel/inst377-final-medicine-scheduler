69c62a2a0373e6d1e99ef88de4e9a3c121be93b0
OrdinaryBagel <159103544+OrdinaryBagel@users.noreply.github.com>
2026-05-16 22:30:06 -0400

    fullcalendar

--- public/Calendar.js
+++ public/Calendar.js
@@ -18,8 +18,25 @@ Schedule(document.getElementById('schedule'), {
 });
 }
 */
-async function createCalender(){
-    const calendarInstance = new calendarJs( "calendar" );
+
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+
+
+function createCalender(){
+        let calendarEl = document.getElementById('calendar');
+let calendar = new Calendar(calendarEl, {
+  plugins: [ dayGridPlugin, timeGridPlugin, listPlugin ],
+  initialView: 'dayGridMonth',
+  headerToolbar: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,listWeek'
+  }
+});
+calendar.render();
 }
 
 window.onload = createCalender;
